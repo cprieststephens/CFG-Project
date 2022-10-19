20ed6636558f0d542f06eff191a76454cf4df803
Camilla Priest-Stephens <cprieststephens@googlemail.com>
2022-10-19 06:27:09 +0100

    add javascript alerts to landing page

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-//Function to create pop up for cart icon
+//Function to create alert for cart icon
 function cartAlert() {
   alert("Your shopping cart is empty");
+}
+
+//Function to create alert for email submit button
+function emailAlert() {
+  alert("Thank you! Your discount code is NEWNOV22");
+}
+
+//Function to create alert for mailing list text
+function discountAlert() {
+  alert("Sign up for our mailing list to receive 10% off your next order!");
 }
