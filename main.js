a2acb35efaef502cf5fe6271e2e7adf89dfe98b0
Camilla Priest-Stephens <cprieststephens@googlemail.com>
2022-10-31 06:34:02 +0000

    change existing javascript alerts to HTML inner text

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 //Function to create alert for cart icon
+let emptyCartMessage = document.getElementById("cart_message");
 function cartAlert() {
-  alert("Your shopping cart is empty");
+  emptyCartMessage.innerText = "Your cart is empty";
 }
 
-//Function to create alert for email submit button
+//Function to create message on click of mailing list submit button
+let mailingListForm = document.getElementById("mailing_list");
 function emailAlert() {
-  alert("Thank you! Your discount code is NEWNOV22");
+  mailingListForm.innerText = "Thank you! Your discount code is NEWNOV22.";
 }
 
 //Function to create alert for mailing list text
+let mailingListPrompt = document.getElementById("mailing_list_prompt");
 function discountAlert() {
-  alert("Sign up for our mailing list to receive 10% off your next order!");
+  mailingListPrompt.innerText =
+    "Sign up for our mailing list to receive 10% off your next order!";
 }
