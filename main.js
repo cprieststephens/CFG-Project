680b6e0c7d5cb988d584642203680bb86c6d61f7
Camilla Priest-Stephens <cprieststephens@googlemail.com>
2022-10-31 13:46:00 +0000

    add javascript validation to mailing list form on landing page

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
-//Function to create alert for cart icon
+//Function to create message on mouseover of cart icon and cart text
 let emptyCartMessage = document.getElementById("cart_message");
-function cartAlert() {
+function cartMessage() {
   emptyCartMessage.innerText = "Your cart is empty";
 }
 
-//Function to create message on click of mailing list submit button
+//Function to create error message or confirmation message on click of mailing list submit button
+let email = document.getElementById("email");
+let emailErrorMessage = document.getElementById("email_error_message");
 let mailingListForm = document.getElementById("mailing_list");
-function emailAlert() {
-  mailingListForm.innerText = "Thank you! Your discount code is NEWNOV22.";
+function emailMessage() {
+  let getEmail = email.value;
+  if (getEmail == "") {
+    emailErrorMessage.innerText = "Please enter your email address";
+  } else {
+    mailingListForm.innerText = "Thank you! Your discount code is TAKE10";
+  }
 }
 
-//Function to create alert for mailing list text
+//Function to create prompt message for mailing list text
 let mailingListPrompt = document.getElementById("mailing_list_prompt");
-function discountAlert() {
+function discountMessage() {
   mailingListPrompt.innerText =
     "Sign up for our mailing list to receive 10% off your next order!";
 }
